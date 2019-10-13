4478256b96c504177a279d1ee3cdca3511c560da
Anthony Dugarte <toonny1998@gmail.com>
2019-10-13 18:00:25 -0400

    Add common used functions

---
+++ lib/pages/auth/common.dart
@@ -0,0 +1,20 @@
+import "package:flutter/material.dart";
+
+Widget padded({Widget child}) => Padding(
+      padding: EdgeInsets.symmetric(vertical: 8.0),
+      child: child,
+    );
+
+Widget scaffolded({Widget child, Widget title}) => Scaffold(
+      appBar: AppBar(
+        title: title,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(32.0),
+        child: child,
+      ),
+    );
+
+Pattern validEmailpattern =
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+RegExp valdEmailRegex = new RegExp(validEmailpattern);
