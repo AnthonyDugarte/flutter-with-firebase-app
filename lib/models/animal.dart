edc50bd89a0a91949a6903504c44697769d31d0f
Anthony Dugarte <toonny1998@gmail.com>
2019-10-22 07:09:05 -0400

    Add Animal constructor from FB DB snapshot

--- lib/models/animal.dart
+++ lib/models/animal.dart
@@ -1,5 +1,13 @@
+import 'package:firebase_database/firebase_database.dart';
+
 class Animal {
   String key, name, age, image;
 
   Animal({this.key, this.name, this.age, this.image});
+
+  Animal.getAnimal(DataSnapshot snap)
+      : key = snap.key,
+        name = snap.value["name"],
+        age = snap.value["age"],
+        image = snap.value["image"];
 }
